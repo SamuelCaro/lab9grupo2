543ab8b80b940176c427de33011716c403520f13
Sebastian-RG <fullmetalliferous@gmail.com>
2020-07-06 19:55:56 -0500

    listen

--- index.js
+++ index.js
@@ -22,4 +22,24 @@ conn.connect(function (err) {
     }
 });
 
+app.post("/centrosPoblados/create", function (request, response) {
+    var nombre = request.body.nombreCentroPoblado;
+    var ubigeo = request.body.ubigeo;
+    console.log("aaaaaaaaaaa");
+    //response.send(`Nombre: ${nombre} | Apellido: ${apellido}`);
+    var sql = "INSERT INTO inventariotest.centrospoblados (nombreCentroPoblado, ubigeo) VALUES (?, ?)";
+    var parametros = [nombre, ubigeo];
+    conn.query(sql, parametros, function (err, resultado) {
+        if (err) {
+            console.log(err);
+        } else {
+            response.json(resultado);
+        }
+    });
+
+});
+
+app.listen(3000, function () {
+    console.log("servidor levantado exitosamente");
+});
 
